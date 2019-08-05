57c263c79d4593dc0c346bf9a5857b3160547b2b
Pedro <pedro.goncalves5493@gmail.com>
2019-08-05 15:58:58 +0100

    - added login Services + Login Business - Finalize login services + business

---
+++ projetoFichaColaborador/src/main/java/io/altar/projetoFichaColaborador/business/LoginBusiness.java
@@ -0,0 +1,36 @@
+package io.altar.projetoFichaColaborador.business;
+
+import javax.ws.rs.core.Response;
+
+import io.altar.projetoFichaColaborador.models.Credentials;
+import io.altar.projetoFichaColaborador.models.User;
+import io.altar.projetoFichaColaborador.repositories.UserRepository;
+
+public class LoginBusiness {
+
+	private UserRepository uR;
+
+	private User currentUser;
+	
+	public User getCurrentUser() {
+		return currentUser;
+	}
+
+	public void setCurrentUser(User currentUser) {
+		this.currentUser = currentUser;
+	}
+
+	public Response getUserLogin(Credentials userCredentials) {
+		User logedUserTry = uR.getUserFromCredentials(userCredentials);
+
+		if (logedUserTry.getId() != 0) {
+			setCurrentUser(logedUserTry);
+			return Response.accepted().entity(logedUserTry).build();
+		} else {
+			return Response.status(Response.Status.NO_CONTENT).entity("Username e/ou Password incorrectos").build();
+		}
+	}
+
+	
+
+}
---
+++ projetoFichaColaborador/src/main/java/io/altar/projetoFichaColaborador/services/LoginServices.java
@@ -0,0 +1,29 @@
+package io.altar.projetoFichaColaborador.services;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.altar.projetoFichaColaborador.business.LoginBusiness;
+import io.altar.projetoFichaColaborador.models.Credentials;;
+
+@Path("login")
+public class LoginServices {
+
+	@Inject
+	private LoginBusiness lB;
+
+	@POST
+	@Path("login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+
+	public Response getUserLogin(Credentials userCredential) {
+		return lB.getUserLogin(userCredential);
+	}
+
+}
