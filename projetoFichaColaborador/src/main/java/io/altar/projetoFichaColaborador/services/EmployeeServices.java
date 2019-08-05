db444ae7ae17094f00be3605c4a6ae7b5fed1eb5
Gabriel <gabrielcemiranda@gmail.com>
2019-08-05 17:19:38 +0100

    Employee endpoints (DELETE & UPDATE)

--- projetoFichaColaborador/src/main/java/io/altar/projetoFichaColaborador/services/EmployeeServices.java
+++ projetoFichaColaborador/src/main/java/io/altar/projetoFichaColaborador/services/EmployeeServices.java
@@ -2,8 +2,10 @@ package io.altar.projetoFichaColaborador.services;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -57,6 +59,24 @@ public class EmployeeServices {
 		
 		eB.createEmployee(employee);
 		return Response.ok().build();
+		
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response removeEmployee(@PathParam("id") long id) {
+		
+		return eB.removeEmployee(id);
+		
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response updateEmployee(Employee employee) {
+		
+		return eB.updateEmployee(employee);
 		
 	}
 	
