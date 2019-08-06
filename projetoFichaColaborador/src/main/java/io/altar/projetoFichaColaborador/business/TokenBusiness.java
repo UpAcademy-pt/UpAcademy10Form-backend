894313d9ce3f23acb1524e0d4dbb495fbecb680b
Gonçalo Monteiro <goncalomonteiro25@gmail.com>
2019-08-06 19:13:43 +0100

    email final

--- projetoFichaColaborador/src/main/java/io/altar/projetoFichaColaborador/business/TokenBusiness.java
+++ projetoFichaColaborador/src/main/java/io/altar/projetoFichaColaborador/business/TokenBusiness.java
@@ -7,15 +7,6 @@ import java.time.Instant;
 import java.util.Base64;
 
 import javax.inject.Inject;
-import javax.ws.rs.core.Response;
-
-import com.sendgrid.Method;
-import com.sendgrid.Request;
-import com.sendgrid.SendGrid;
-import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.Content;
-import com.sendgrid.helpers.mail.objects.Email;
-
 import io.altar.projetoFichaColaborador.models.Token;
 import io.altar.projetoFichaColaborador.repositories.TokenRepository;
 
@@ -24,19 +15,24 @@ public class TokenBusiness {
 @Inject
 private TokenRepository tR;
 
+@Inject
+private EmailBusiness eB;
+
+
+
 private static final SecureRandom secureRandom = new SecureRandom(); // threadsafe
 private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder(); // threadsafe
 
-public Token generateNewToken(String Email) throws IOException {
+public Token generateNewToken(String email) throws IOException {
     byte[] randomBytes = new byte[16];
     secureRandom.nextBytes(randomBytes);
     String value = base64Encoder.encodeToString(randomBytes);
-    String employeeEmail = Email;
+    String employeeEmail = email;
     Long timeToLive = Instant.now().toEpochMilli()+86400000;//24h to milliseconds
     //Long timeToLive = Instant.now().toEpochMilli() + 10;
     Token Token = new Token(value, employeeEmail, timeToLive);
     Token tokenSaved = tR.create(Token);
-    sendEmail(employeeEmail,value);
+    eB.sendEmail(employeeEmail,value);
     return tokenSaved;
 }
 
@@ -74,27 +70,27 @@ public void removeToken(String tokenValue) {
 	return;
 }
 
-public void sendEmail(String email,String token) throws IOException {
-    Email from = new Email("newcolaborador@gmail.com");
-    String subject = "Registo Ficha Colaborador - Projeto UpAcademy";
-    Email to = new Email(email);
-    Content content = new Content("text/plain", "Registe no link: exemplolink: url?"+ token);
-    Mail mail = new Mail(from, subject, to, content);
-
-    SendGrid sg = new SendGrid(System.getProperty("SGKey"));
-    Request request = new Request();
-    try {
-      request.setMethod(Method.POST);
-      request.setEndpoint("mail/send");
-      request.setBody(mail.build());
-      com.sendgrid.Response response = sg.api(request);
-      System.out.println(response.getStatusCode());
-      System.out.println(response.getBody());
-      System.out.println(response.getHeaders());
-    } catch (IOException ex) {
-      throw ex;
-    }
-  }
+//public void sendEmail(String email,String token) throws IOException {
+//    Email from = new Email("newcolaborador@gmail.com");
+//    String subject = "Registo Ficha Colaborador - Projeto UpAcademy";
+//    Email to = new Email(email);
+//    Content content = new Content("text/plain", "Registe no link: exemplolink: url?"+ token);
+//    Mail mail = new Mail(from, subject, to, content);
+//
+//    SendGrid sg = new SendGrid(System.getProperty("SGKey"));
+//    Request request = new Request();
+//    try {
+//      request.setMethod(Method.POST);
+//      request.setEndpoint("mail/send");
+//      request.setBody(mail.build());
+//      Response response = sg.api(request);
+//      System.out.println(response.getStatusCode());
+//      System.out.println(response.getBody());
+//      System.out.println(response.getHeaders());
+//    } catch (IOException ex) {
+//      throw ex;
+//    }
+//  }
 
 
 
